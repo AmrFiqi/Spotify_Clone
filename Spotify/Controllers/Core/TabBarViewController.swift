22ae74f8de5ae4483c1f1861ab7da90223468be5
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-05-29 00:35:54 +0300

    Condition to show home screen only when logged in

--- Spotify/Controllers/Core/TabBarViewController.swift
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -31,6 +31,7 @@ class TabBarViewController: UITabBarController {
         let searchNav = UINavigationController(rootViewController: searchVC)
         let libraryNav = UINavigationController(rootViewController: libraryVC)
         
+        // Set Tab Bar icons
         homeNav.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
         searchNav.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
         libraryNav.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1 )
