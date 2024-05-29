72de8aa2c4b3db32c7a57770a9f87ad83f8ceccc
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-05-29 12:07:04 +0300

    Setup welcome screen with sign in button

--- Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Controllers/Other/AuthViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import WebKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, WKNavigationDelegate {
+    
+    // MARK: - Variables
+    
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        let webView = WKWebView(frame: .zero, configuration: config)
+        
+        return webView
+    }()
+    
+    public var completionHandler: ((Bool) -> Void)?
+    
+    // MARK: - Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Sign In"
+        view.backgroundColor = .white
+        webView.navigationDelegate = self
+        view.addSubview(webView)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
 }
--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    // MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Spotify"
         view.backgroundColor = .green
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTabSignIn), for: .touchUpInside)
     }
    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+//        signInButton.frame = CGRect(x: 30,
+//                                    y: view.height-75-view.safeAreaInsets.bottom,
+//                                    width: view.width-70,
+//                                    height: 50)
+        signInButton.translatesAutoresizingMaskIntoConstraints = false // Enable Auto Layout
 
+            NSLayoutConstraint.activate([
+              signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              signInButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50), // Adjust vertical offset as needed
+              signInButton.widthAnchor.constraint(equalToConstant: view.width - 70), // Maintain existing width
+              signInButton.heightAnchor.constraint(equalToConstant: 50) // Maintain existing height
+            ])
+
+    }
+    
+    @objc func didTabSignIn() {
+        let vc = AuthViewController()
+        vc.completionHandler = {[weak self] success in
+            self?.handleSignIn(success: success)
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        // Log user in
+    }
 }
