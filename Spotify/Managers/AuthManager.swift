76f8166b5a75fa88a371dafeb2651137849b2cf3
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-05-29 11:42:10 +0300

    Setup Auth manager

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -7,11 +7,35 @@
 
 import Foundation
 
+
 final class AuthManager {
     static let shared = AuthManager()
     
     private init() {}
+   
+    private static let plistName = "Secrets"
+    private static let clientIdKey = "CLIENT_ID"
+    private static let clientSecretKey = "CLIENT_SECRET"
     
+    static func getClientId() -> String? {
+        guard let filePath = Bundle.main.path(forResource: plistName, ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath) else {
+            return nil
+        }
+        return plist[clientIdKey] as? String
+    }
+    
+    static func getClientSecret() -> String? {
+        guard let filePath = Bundle.main.path(forResource: plistName, ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath) else {
+            return nil
+        }
+        print(plist[clientIdKey] ?? "None")
+        print(plist[clientSecretKey] ?? "None")
+        return plist[clientSecretKey] as? String
+    }
+    
+
     var isSignedIn: Bool {
         return false
     }
@@ -32,3 +56,6 @@ final class AuthManager {
         return false
     }
 }
+
+let clientID = AuthManager.getClientSecret()
+
